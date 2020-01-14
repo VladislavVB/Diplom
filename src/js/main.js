b5255d63b0c394d32a10bad8814fcf66c085dba5
VladislavVB <burdukovskij.vlad@mail.ru>
2020-01-14 21:45:20 +0300

    add section: about, hero, factory, catolog, assumpitin, adventages, about

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+
+    //слайд
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+});
